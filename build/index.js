04270ee0e24fb64dc11dbc266a36c15c74087d6a
Anton <anton@adc.sh>
2019-01-24 01:52:31 +0300

    b

--- build/index.js
+++ build/index.js
@@ -3,9 +3,12 @@ const $wrote_write = require('@wrote/write');
 const $wrote_ensure_path = require('@wrote/ensure-path');
 const $wrote_rm = require('@wrote/rm');
 const $wrote_read_dir_structure = require('@wrote/read-dir-structure');
+const $wrote_exists = require('@wrote/exists');
 
 module.exports.read = $wrote_read
+module.exports.readBuffer = $wrote_read.readBuffer
 module.exports.write = $wrote_write
 module.exports.ensurePath = $wrote_ensure_path
 module.exports.rm = $wrote_rm
 module.exports.readDirStructure = $wrote_read_dir_structure
+module.exports.exists = $wrote_exists
